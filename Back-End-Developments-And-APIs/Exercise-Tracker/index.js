6a9f486414bab7f72636dcdc802021be5683a660
Keith Kong <keithkong1996@gmail.com>
2024-07-14 17:46:37 +1000

    add Exercise Tracker

--- Back-End-Developments-And-APIs/Exercise-Tracker/index.js
+++ Back-End-Developments-And-APIs/Exercise-Tracker/index.js
@@ -10,7 +10,7 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, {});
 
 const exerciseSchema = new mongoose.Schema({
   description: String,
